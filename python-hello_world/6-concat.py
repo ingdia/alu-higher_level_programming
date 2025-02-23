33a2576408d0f7372488aca67bf33124ee55079b
ingdia <d.ingabire2@alustudent.com>
2025-02-23 10:11:21 -0800

    concatinating strings

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#concatinating strings
+# This script prints a welcome message
 str1 = "Holberton"
 str2 = "School"
-print("Welcome to " + str1 + " " + str2 + "!")
+print("Welcome to {} {}!".format(str1, str2))
