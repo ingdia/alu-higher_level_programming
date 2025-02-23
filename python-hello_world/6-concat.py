52dd9c307d75c22f59fe8ea4dea23251b161eebd
ingdia <d.ingabire2@alustudent.com>
2025-02-23 10:27:10 -0800

    concatinating strings

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-# This line prints the welcome message
-print(f"Welcome to {str1} {str2}!")
+str1= (str1 + str2)
+print(f"Welcome to {str1}!")
