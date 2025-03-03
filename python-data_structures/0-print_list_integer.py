6179116bf0ae5ae08ea9a95b0aec7f912a78e787
ingdia <d.ingabire2@alustudent.com>
2025-03-03 11:36:38 -0800

    function that retrieves an element from a list like in C

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    if idx >= len(my_list):
-        return None
-    return my_list[idx]
+def print_list_integer(my_list=[]):
+    for number in my_list:
+        print("{:d}".format(number))
