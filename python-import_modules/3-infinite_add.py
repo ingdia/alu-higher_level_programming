45fe2df8cdd730ca6104bbe5f02e54b462e2d9ca
ingdia <d.ingabire2@alustudent.com>
2025-03-03 11:06:25 -0800

    infinite loop

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -5,14 +5,14 @@ import sys
 def infinite_add():
     # Initialize sum to 0
     total = 0
-    
+
     # Get all arguments (excluding the script name)
     args = sys.argv[1:]
-    
+
     # Add each argument to the total
     for arg in args:
         total += int(arg)
-    
+
     # Print the result
     print(total)
 
