2f19148e440a57bcdff21c64802cae758e047ac3
ingdia <d.ingabire2@alustudent.com>
2025-03-03 14:00:11 -0800

    matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-       if not matrix:
-        return []
-
-    new_matrix = []
-    for row in matrix:
-        new_row = []
-        for element in row:
-            new_row.append(element ** 2)
-        new_matrix.append(new_row)
-    return new_matrix
+    return [[num ** 2 for num in row] for row in matrix]
