88e129bd52708834d3c02d05eca451563979e6ea
ingdia <d.ingabire2@alustudent.com>
2025-03-03 05:02:48 -0800

    print alphabt except q aand e

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-print("".join("{:c}".format(i) for i in range(ord('a'), ord('z') + 1) if chr(i) != 'q' and chr(i) != 'e'))
+print("".join(
+    "{:c}".format(i) for i in range(ord('a'), ord('z') + 1)
+    if chr(i) != 'q' and chr(i) != 'e'
+))
