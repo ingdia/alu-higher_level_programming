267d8715d0b818ee445ad85788b0cc1e98dc6df5
ingdia <d.ingabire2@alustudent.com>
2025-03-03 06:56:55 -0800

    a program that prints the ASCII alphabet, in lowercas

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-print("".join(
-    "{:c}".format(i) for i in range(ord('a'), ord('z') + 1)
-    if chr(i) != 'q' and chr(i) != 'e'
-))
+for i in range(97, 123):
+    if chr(i) not in ['q', 'e']:
+        print("{}".format(chr(i)), end="")
