73035fd4c35ccef24e12aafd13de70f444240a85
ingdia <d.ingabire2@alustudent.com>
2025-03-03 13:45:38 -0800

    matrix

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+     if not matrix:
+        return []
+
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for element in row:
+            new_row.append(element ** 2)
+        new_matrix.append(new_row)
+    return new_matrix
