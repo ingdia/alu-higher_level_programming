ab157362a219edb4404bc93a2e98cda02d89e736
ingdia <d.ingabire2@alustudent.com>
2025-03-04 12:40:38 -0800

    Divide a list

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
-def safe_print_division(a, b):
-    result = None
-    try:
-        result = a / b
-    except ZeroDivisionError:
-        pass
-    finally:
-        print("Inside result: {}".format(result))
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            div = 0
+            if i < len(my_list_1) and i < len(my_list_2):
+                if not (isinstance(my_list_1[i], (int, float)) and
+                        isinstance(my_list_2[i], (int, float))):
+                    print("wrong type")
+                elif my_list_2[i] == 0:
+                    print("division by 0")
+                else:
+                    div = my_list_1[i] / my_list_2[i]
+            else:
+                print("out of range")
+        except TypeError:
+            print("wrong type")
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+        finally:
+            result.append(div)
     return result
-
