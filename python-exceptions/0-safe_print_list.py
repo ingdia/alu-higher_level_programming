25e11cde1fd77b806fcb4076c86c954c1beac3f3
ingdia <d.ingabire2@alustudent.com>
2025-03-04 17:59:40 -0800

    Safe list printing

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-    count = 0
+     count = 0
     try:
         for i in range(x):
             print(my_list[i], end="")
@@ -11,4 +10,3 @@ def safe_print_list(my_list=[], x=0):
     finally:
         print()
     return count
-
