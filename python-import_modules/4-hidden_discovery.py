07cd75b7c40b23fd5a6928e14cddc0821caab5f2
ingdia <d.ingabire2@alustudent.com>
2025-03-07 00:13:01 -0800

    hidden_discovery

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
-def print_names():
-    # Get all names defined in the module
-    names = dir(hidden_4)
-    # Filter out names starting with '__' and sort alphabetically
-    filtered_names = sorted([name for name in names 
-                            if not name.startswith('__')])
-    # Print each name
-    for name in filtered_names:
-        print(name)
 if __name__ == "__main__":
-    print_names()
+    from hidden_4 import *
+    allf = dir()
+    for i in range(0, len(allf)):
+        if allf[i][:2] != "__":
+            print("{:s}".format(allf[i]))
