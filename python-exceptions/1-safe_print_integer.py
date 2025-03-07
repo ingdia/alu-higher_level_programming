4b4b01c13eef1819a7de9396283cdbd50158a21c
ingdia <d.ingabire2@alustudent.com>
2025-03-07 00:59:14 -0800

    print

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    """
-    Prints an integer with "{:d}".format()
-
-    Args:
-        value: can be any type (integer, string, etc.)
-
-    Returns:
-        True if value has been correctly printed (it means the value is an integer)
-        Otherwise, returns False
-    """
     try:
         print("{:d}".format(value))
         return True
--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,25 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    """
-    Prints the first x elements of a list and only integers.
-
-    Args:
-        my_list: can contain any type (integer, string, etc.)
-        x: number of elements to access in my_list
-
-    Returns:
-        The real number of integers printed
-    """
-    count = 0
-    try:
-        for i in range(x):
-            try:
+    printed = 0
+    for i in range(x):
+        try:
+            if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end="")
-                count += 1
-            except (ValueError, TypeError):
-                continue
-        print()
-    except IndexError:
-        print()
-
-    return count
+                printed += 1
+        except IndexError:
+            raise IndexError("list index out of range")
+    print()
+    return printed
