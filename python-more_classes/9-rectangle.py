bb5cfe6324cf417569a6738f22580827a2f14381
ingdia <d.ingabire2@alustudent.com>
2025-03-07 01:27:16 -0800

    removing errors

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -7,6 +7,7 @@ It includes methods to calculate the area and perimeter, as well as
 methods for string representation and comparison of rectangle instances.
 """
 
+
 class Rectangle:
     """A class that defines a rectangle by its width and height.
 
@@ -18,7 +19,7 @@ class Rectangle:
     """
 
     number_of_instances = 0  # Class attribute to count instances
-    print_symbol = "#"  # Class attribute for the symbol used in string representation
+    print_symbol = "#"  # Class attribute for the symbol used in string 
 
     def __init__(self, width=0, height=0):
         """Initializes a new Rectangle instance.
@@ -112,7 +113,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(
-            [str(self.print_symbol) * self.__width 
+            [str(self.print_symbol) * self.__width
              for _ in range(self.__height)]
         )
 
@@ -147,7 +148,7 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        
+
         if rect_1.area() >= rect_2.area():
             return rect_1
         return rect_2
@@ -160,6 +161,6 @@ class Rectangle:
             size (int, optional): The size of the square. Defaults to 0.
 
         Returns:
-            Rectangle: A new Rectangle instance with width and height equal to size.
+            Rectangle: A new Rectangle instance with width and height
         """
         return cls(size, size)
