d6b64b3b2582caf1da654db4e346add39358584f
ingdia <d.ingabire2@alustudent.com>
2025-03-07 00:50:32 -0800

    roman

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,27 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-str = (str[39:66] + ' ' + str[-22:-18] + ' ' + str[:6])
-print(str)
-Tabitha Akimana
-10:10 AM
-#!/usr/bin/python3
-if __name__ == "__main__":
-    from hidden_4 import *
-    allf = dir()
-    for i in range(0, len(allf)):
-        if allf[i][:2] != "__":
-            print("{:s}".format(allf[i]))
-Tabitha Akimana
-10:20 AM
-#!/usr/bin/python3
-def delete_at(my_list=[], idx=0):
-    if 0 <= idx < len(my_list):
-        del my_list[idx]
-    return my_list
-Tabitha Akimana
-10:30 AM
-#!/usr/bin/python3
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
@@ -40,3 +17,6 @@ def roman_to_int(roman_string):
             total -= current_value
         else:
             total += current_value
+        prev_value = current_value
+
+    return total
