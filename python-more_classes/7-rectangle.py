6f2a62287fefead5a613bb42780e6b82a1ff9377
ingdia <d.ingabire2@alustudent.com>
2025-03-07 01:41:14 -0800

    rectangel7

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -107,7 +107,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(
-            [str(self.print_symbol) * self.__width 
+            [str(self.print_symbol) * self.__width
              for _ in range(self.__height)]
         )
 
