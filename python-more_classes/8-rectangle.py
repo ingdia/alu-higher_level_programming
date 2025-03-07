bf13c071f0a2e9cb62dd8f294648bd7b9852c8cd
ingdia <d.ingabire2@alustudent.com>
2025-03-06 22:06:31 -0800

    rectangle8

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -139,10 +139,4 @@ class Rectangle:
             Rectangle: The rectangle with the larger area, or rect_1 if equal.
         """
         if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        
-        if rect_1.area() >= rect_2.area():
-            return rect_1
-        return rect_2
+            raise TypeError("rect_1 must be an instance
