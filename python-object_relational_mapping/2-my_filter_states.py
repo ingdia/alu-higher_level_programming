1aa14ea990c2f532b56deb8e410c4dc170b1afeb
ingdia <d.ingabire2@alustudent.com>
2025-04-05 02:59:49 -0700

    object reletion mapping

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-"""Define the State class for SQLAlchemy."""
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+# script that takes in an argument and displays all values,
+# in the 'states' table of 'hbtn_0e_0_usa' where 'name' matches the argument
+"""
+   import 'sys' & 'MySQLdb'
+"""
+import sys
+import MySQLdb
 
-Base = declarative_base()
-
-class State(Base):
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    name = Column(String(128), nullable=False)
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
