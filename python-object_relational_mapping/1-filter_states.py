7bb95126bde00429b579cf44c1daddd0b4523511
ingdia <d.ingabire2@alustudent.com>
2025-04-05 02:53:41 -0700

    object reletion mapping

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python3
-import MySQLdb
+"""
+    Create a script that lists all states from a database hbtn_0e_0_usa
+"""
+
 import sys
+import MySQLdb
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    for row in cursor.fetchall():
-        print(row)
-    cursor.close()
-    db.close()
+    db_conn = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        host='localhost',
+        port=3306
+    )
+
+    cur = db_conn.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
+
+    cur.close()
+    db_conn.close()
