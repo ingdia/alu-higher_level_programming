d9ae8bcebd5f320ddd27884050caed2d2922e319
ingdia <d.ingabire2@alustudent.com>
2025-04-09 07:51:46 -0700

    More queries

---
+++ SQL_more_queries/create_users.sql
@@ -0,0 +1,5 @@
+-- This script lists all privileges of the MySQL users user_0d_1 and user_0d_2
+CREATE USER 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+CREATE USER 'user_0d_2'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
