f219d5b547dc9ddb16a41e13184fb6a905522aeb
Diane Ingabire <d.ingabire2@alustudent.com>
2025-05-31 07:39:08 +0000

    argument counts

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -4,8 +4,8 @@ if (args.length === 0){
     console.log('No argument');
 }
 else if (args.length === 1){
-    console.log('Argument Found');
+    console.log('Argument found');
 }
 else{
     console.log('Arguments found');
-}
+};
