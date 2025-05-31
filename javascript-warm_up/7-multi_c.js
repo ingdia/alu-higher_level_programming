2eae234d3cf51915e30442edef0f0f1c122657a6
Diane Ingabire <d.ingabire2@alustudent.com>
2025-05-31 13:06:15 +0000

    correcting errors in file 7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -11,4 +11,17 @@ if (isNaN(x)) {
         i++;
 }
 }
+#!/usr/bin/node
+
+const x = parseInt(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+  while (i < x) { 
+    console.log('C is fun');
+    i++;
+  }
+}
 
