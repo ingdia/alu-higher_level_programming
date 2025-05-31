b0b1f5909106876a27e4e7f7579a70acf999e11e
Diane Ingabire <d.ingabire2@alustudent.com>
2025-05-31 20:07:24 +0000

    fixindg errors in file 8

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-function reversemylist (list) {
+exports.esrever = function (list) {
   const reversedlist = [];
-  for (let i = list.lenght - 1; i >= 0; i--) {
+  for (let i = list.length - 1; i >= 0; i--) {
     reversedlist.push(list[i]);
   }
   return reversedlist;
-}
-module.exports = reversemylist;
+};
+
