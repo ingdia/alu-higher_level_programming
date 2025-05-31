4412694722b562d8bba74b20ea5500d4dfa1b806
Diane Ingabire <d.ingabire2@alustudent.com>
2025-05-31 13:40:15 +0000

    fix errors

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+if (args.length < 2) {
+  console.log(0);
+} else {
+  const nums = [];
+
+  for (let i = 0; i < args.length; i++) {
+    nums.push(parseInt(args[i]));
+  }
+
+  nums.sort(function (a, b) {
+    return b - a;
+  });
+
+  console.log(nums[1]);
+}
