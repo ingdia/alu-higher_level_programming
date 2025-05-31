c125dca310f8741f7dcec5269c5fb771bb6f81a3
Diane Ingabire <d.ingabire2@alustudent.com>
2025-05-31 12:18:44 +0000

    function to add

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -3,13 +3,11 @@
 let num1 = parseInt(process.argv[2])
 let num2 = parseInt(process.argv[3])
 
-if (isNaN(num1)|| isNaN(num2)){
-    console.log(num1)
-} else {
+
     function add(a,b){
-    return a+b
+    return a + b
     }
     console.log(add(num1, num2))
-}
+
 
 
