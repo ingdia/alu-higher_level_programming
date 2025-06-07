f4d86b46077e0db6a756e2ce52e8fb437b197358
Diane Ingabire <d.ingabire2@alustudent.com>
2025-06-07 06:51:23 +0000

    semistandard fix for file 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
 
-const fs = require('fs')
+const fs = require('fs');
 
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf-8',(err,data) => {
-    if(err) {
-        console.log(err);
-
-    } else {
-        console.log(data);
-    }
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 }
 
 );
